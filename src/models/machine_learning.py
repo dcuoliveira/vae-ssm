0008d3230c886fb410dd534c4acc99f070a451c6
dcuoliveira <dan.cunhaa@gmail.com>
2023-03-30 17:18:32 -0300

    refactor ml methods

--- src/models/machine_learning.py
+++ src/models/machine_learning.py
@@ -1,7 +1,6 @@
 from scipy.stats import randint as sp_randint
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.neural_network import MLPRegressor
 
 
 class RandomForestWrapper():
@@ -16,20 +15,3 @@ class RandomForestWrapper():
             self.ModelClass = RandomForestRegressor()
         else:
             self.ModelClass = RandomForestRegressor(**model_params)
-
-
-class NN3Wrapper():
-    def __init__(self, model_params=None):
-        self.model_name = "nn3"
-        self.search_type = 'random'
-        self.param_grid = {"early_stopping": [True],
-                           "learning_rate": ["invscaling"],
-                           "learning_rate_init": np.linspace(0.001, 0.999, 100),
-                           'alpha': np.linspace(0.001, 0.999, 100),
-                           'solver': ["adam"],
-                           'activation': ["relu"],
-                           "hidden_layer_sizes": [(32, 16, 8)]}
-        if model_params is None:
-            self.ModelClass = MLPRegressor()
-        else:
-            self.ModelClass = MLPRegressor(**model_params)
