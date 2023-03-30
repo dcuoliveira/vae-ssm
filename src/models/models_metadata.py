b5c8f6f869f46e0f67fd268ce54e3b9050df9f4b
dcuoliveira <dan.cunhaa@gmail.com>
2023-03-30 17:18:42 -0300

    first commit models metadata

---
+++ src/models/models_metadata.py
@@ -0,0 +1,9 @@
+from models.machine_learning import *
+from models.deep_learning import *
+
+
+models_metadata = {
+    
+    "rf": RandomForestWrapper,
+    "simple_mlp":  MLPRegressor,
+                   }
