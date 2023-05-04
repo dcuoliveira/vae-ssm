f870a7879cab2603f185fbae072215fe9b16adc5
dcuoliveira <dan.cunhaa@gmail.com>
2023-05-04 20:31:36 -0300

    add vrnn

--- src/models/models_metadata.py
+++ src/models/models_metadata.py
@@ -1,9 +1,11 @@
 from models.MLP import *
 from models.RandomForest import *
+from models.VRNN import *
 
 
 models_metadata = {
     
     "rf": RandomForestWrapper,
     "simple_mlp":  NN3Wrapper,
+    "vrnn": VRNN,
                    }
