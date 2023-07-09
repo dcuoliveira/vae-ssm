05becda141354081973d95b173fb2caffb0853bf
dcuoliveira <dan.cunhaa@gmail.com>
2023-07-09 18:12:15 -0300

    add new rolling window funcs

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,15 +1,44 @@
 import torch
 import numpy as np
-import pandas as pd
 
-def create_ts_prediction_data(data, seq_length):
+def create_rolling_indices(num_timesteps_in, num_timesteps_out, n_timesteps, fix_start):
     
-    X = []
-    for i in range(len(data) - seq_length - 1):
-        _x = data[i:(i + seq_length)]
-        X.append(_x)
+    # generate rolling window indices
+    indices = [
+        (0 if fix_start else i, i + (num_timesteps_in + num_timesteps_out))
+        for i in range(n_timesteps - (num_timesteps_in + num_timesteps_out) + 1)
+    ]
 
-    return np.array(X)
+    return indices
+
+def create_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_out, fix_start=False):
+        
+    if features.shape[0] != target.shape[0]:
+        raise Exception("Features and target must have the same number of timesteps")
+
+    n_timesteps = features.shape[0]
+    indices = create_rolling_indices(num_timesteps_in=num_timesteps_in,
+                                     num_timesteps_out=num_timesteps_out,
+                                     n_timesteps=n_timesteps,
+                                     fix_start=fix_start)
+    
+    # use rolling window indices to subset data
+    window_features, window_target = [], []
+    for i, j in indices:
+        window_features.append(np.array(features[i:(i + num_timesteps_in), :]))
+        window_target.append(np.array(target[(i + num_timesteps_in - 1):j, :]))
+
+    return torch.tensor(window_features), torch.tensor(window_target)
+
+def timeseries_train_test_split(X, y, train_ratio):
+    train_ratio = int(len(X) * train_ratio)
+    
+    X_train = X[:train_ratio, :]
+    y_train = y[:train_ratio, :]
+    X_test = X[train_ratio:, :]
+    y_test = y[train_ratio:, :]
+
+    return X_train, X_test, y_train, y_test
 
 def regular_numeric_scaler(data):
     pass
