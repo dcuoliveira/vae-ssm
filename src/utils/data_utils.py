7c22d10a93109f459c90fc3be0e26480ea62162f
dcuoliveira <dan.cunhaa@gmail.com>
2023-10-04 21:28:09 -0300

    add funcs

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -24,9 +24,43 @@ def create_rolling_indices(num_timesteps_in, num_timesteps_out, n_timesteps, fix
 
     return indices
 
-def timeseries_train_test_split_online(X, y, train_ratio):
-    train_size = int(X.shape[0] * train_ratio)
+def create_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_out, fix_start=False, drop_last=True):
+    """"
+    This function is used to create the rolling window time series to be used on DL ex-GNN.
 
+    One important thing to note is that, since we are in the context of sharpe ratio optimization,
+    and we are assuming positions are being taken at the close price of the same day, we have to 
+    increase our target variable (prices) by one time step so as to compute the sharpe ratio properly.
+    """
+        
+    if features.shape[0] != target.shape[0]:
+        raise Exception("Features and target must have the same number of timesteps")
+
+    n_timesteps = features.shape[0]
+    indices = create_rolling_indices(num_timesteps_in=num_timesteps_in,
+                                     num_timesteps_out=num_timesteps_out,
+                                     n_timesteps=n_timesteps,
+                                     fix_start=fix_start)
+    
+    # use rolling window indices to subset data
+    window_features, window_target = [], []
+    for i, j in indices:
+        window_features.append(features[i:j, :])
+        window_target.append(target[(i + 1):(j + num_timesteps_out), :])
+
+    if drop_last:
+        window_features = window_features[:-1]
+        window_target = window_target[:-1]
+
+    return torch.stack(window_features), torch.stack(window_target)
+
+def timeseries_train_test_split(X, y, train_ratio):
+
+    if X.shape[0] != y.shape[0]:
+        raise Exception("Features and target must have the same number of timesteps")
+    
+    train_size = int(len(X) * train_ratio)
+    
     X_train = X[:train_size, :]
     y_train = y[:train_size, :]
     X_test = X[train_size:, :]
@@ -63,35 +97,6 @@ def create_online_rolling_window_ts(target, features, num_timesteps_in, num_time
         window_target = window_target[:-1]
 
     return torch.stack(window_features), torch.stack(window_target)
-
-def create_rolling_window_ts(target, features, num_timesteps_in, num_timesteps_out, fix_start=False):
-        
-    if features.shape[0] != target.shape[0]:
-        raise Exception("Features and target must have the same number of timesteps")
-
-    n_timesteps = features.shape[0]
-    indices = create_rolling_indices(num_timesteps_in=num_timesteps_in,
-                                     num_timesteps_out=num_timesteps_out,
-                                     n_timesteps=n_timesteps,
-                                     fix_start=fix_start)
-    
-    # use rolling window indices to subset data
-    window_features, window_target = [], []
-    for i, j in indices:
-        window_features.append(np.array(features[i:(i + num_timesteps_in), :]))
-        window_target.append(np.array(target[(i + num_timesteps_in - 1):j, :]))
-
-    return torch.tensor(window_features), torch.tensor(window_target)
-
-def timeseries_train_test_split(X, y, train_ratio):
-    train_ratio = int(len(X) * train_ratio)
-    
-    X_train = X[:train_ratio, :]
-    y_train = y[:train_ratio, :]
-    X_test = X[train_ratio:, :]
-    y_test = y[train_ratio:, :]
-
-    return X_train, X_test, y_train, y_test
 
 def regular_numeric_scaler(data):
     pass
